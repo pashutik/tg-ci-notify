5089c95658ec82db7301471e8da570d62ba28e08
pashutik <pavel.buchinskii@gmail.com>
2018-12-19 13:34:03 +0200

    create notify script v2.0

--- .ci-notify.sh
+++ .ci-notify.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
-
+ 
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT_SUCCESS="\
+Deploy status: Successfull✅%0A%0A\
+Project:+$CI_PROJECT_NAME%0A\
+URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
+Branch:+$CI_COMMIT_REF_SLUG%0A\
+Commit:+$CI_COMMIT_MESSAGE%0A\
+Commiter:+$GITLAB_USER_NAME"
+ 
+TEXT_FAILE="\
+Deploy status: Failed❌%0A%0A\
+Project:+$CI_PROJECT_NAME%0A\
+URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
+Branch:+$CI_COMMIT_REF_SLUG%0A\
+Commit:+$CI_COMMIT_MESSAGE%0A\
+Commiter:+$GITLAB_USER_NAME"
 
-curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+if [[ $1 == "faile" ]];
+    then
+        for TELEGRAM_USER_ID in $USERS_FOR_NOTIFY;
+            do
+                curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT_FAILE" $URL > /dev/null
+            done
+    elif [[ $1 == "success" ]];
+        then
+        for TELEGRAM_USER_ID in $USERS_FOR_NOTIFY;
+            do
+                curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT_SUCCESS" $URL > /dev/null
+            done
+fi
+exit
