dd8a13124a7e409295340dd02e2b65c397fd723e
pashutik <pavel.buchinskii@gmail.com>
2018-12-19 13:39:23 +0200

    update notify script to v2.1

--- .ci-notify.sh
+++ .ci-notify.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
- 
-TIME="10"
-URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT_SUCCESS="\
+
+t="10"
+
+url="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+
+text_start="\
+👻Start of Pipeline👻%0A\
+Project:+$CI_PROJECT_NAME%0A\
+URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
+Branch:+$CI_COMMIT_REF_SLUG%0A\
+Commit:+$CI_COMMIT_MESSAGE%0A\
+Commiter:+$GITLAB_USER_NAME"
+
+text_success="\
 Deploy status: Successfull✅%0A%0A\
 Project:+$CI_PROJECT_NAME%0A\
 URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
 Branch:+$CI_COMMIT_REF_SLUG%0A\
 Commit:+$CI_COMMIT_MESSAGE%0A\
 Commiter:+$GITLAB_USER_NAME"
- 
-TEXT_FAILE="\
+
+text_faile="\
 Deploy status: Failed❌%0A%0A\
 Project:+$CI_PROJECT_NAME%0A\
 URL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0A\
@@ -18,17 +28,15 @@ Branch:+$CI_COMMIT_REF_SLUG%0A\
 Commit:+$CI_COMMIT_MESSAGE%0A\
 Commiter:+$GITLAB_USER_NAME"
 
-if [[ $1 == "faile" ]];
-    then
-        for TELEGRAM_USER_ID in $USERS_FOR_NOTIFY;
-            do
-                curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT_FAILE" $URL > /dev/null
-            done
-    elif [[ $1 == "success" ]];
-        then
-        for TELEGRAM_USER_ID in $USERS_FOR_NOTIFY;
-            do
-                curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT_SUCCESS" $URL > /dev/null
-            done
-fi
-exit
+for i in $TELEGRAM_CHANEL_ID;
+	do
+		case $1 in
+			start)
+				curl -s --max-time $t -d "chat_id=$i&disable_web_page_preview=1&text=$text_start" $url > /dev/null;;
+			faile)
+				curl -s --max-time $t -d "chat_id=$i&disable_web_page_preview=1&text=$text_faile" $url > /dev/null;;
+			success)
+				curl -s --max-time $t -d "chat_id=$i&disable_web_page_preview=1&text=$text_success" $url > /dev/null;;
+		esac
+	done
+exit 0
